9203cd040a4a26bb1097cb10c177f069a556a714
Gürkan N. Kalkan <nesimigurkankalkan@gmail.com>
2025-11-29 15:22:34 +0300

    IServices created

---
+++ Core/Abstracts/IServices/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Concretes.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Abstracts.IServices
+{
+    public interface IProductService
+    {
+        ProductDetailDTO GetProductById(int productId);
+        IEnumerable<ProductListItemDTO> GetAllProducts();
+
+    }
+}
---
+++ Core/Concretes/DTOs/ProductDTOs.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Concretes.DTOs
+{
+    //Ürün listelemek için kullanıyoruz
+    public class ProductListItemDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal DiscountedRate { get; set; }
+        public bool IsStock { get; set; }
+        public decimal Rating { get; set; }
+        public string Origin { get; set; }
+        public string RoastLevel { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+    //Ürün detayları için kullanıyoruz
+    public class ProductDetailDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SKU { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal DiscountedRate { get; set; }
+        public int StockQuantity { get; set; }
+        public string Origin { get; set; }
+        public string RoastLevel { get; set; }
+        public string FlavorNotes { get; set; }
+        public decimal? Rating { get; set; }
+        public bool IsFeatured { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int SubCategoryId { get; set; }
+        public string SubCategoryName { get; set; }
+
+        public IEnumerable<string> ImageUrls { get; set; } = new HashSet<string>();
+        public IEnumerable<ReviewDTO> Reviews { get; set; } = new HashSet<ReviewDTO>();
+    }
+
+    public class  ReviewDTO
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+
+}
